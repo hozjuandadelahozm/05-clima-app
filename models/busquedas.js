18b1830d4b2e50346fd235e0804fc888182f3f46
Juan David De la hoz <hozjuanda11@gmail.com>
2022-04-12 23:08:06 -0500

    inicio app clima

---
+++ models/busquedas.js
@@ -0,0 +1,22 @@
+
+
+class Busquedas {
+
+    historial = [ 'Tegucigalpa', 'Madrid', 'San José' ];
+
+    constructor() {
+        // TODO: Leer DB si existe
+    }
+
+    async ciudad( lugar = '' ) {
+
+        // peticion http
+        console.log( lugar );
+
+        return[]; // retornar los lugares
+
+    }
+
+}
+
+module.exports = Busquedas;
